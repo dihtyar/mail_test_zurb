69801e6d57793b31761532ec88b57f5aae9d61b8
Bogdan Dikhtiar <bogdandihtyar@gmail.com>
2023-03-27 13:00:19 +0300

    added gictures in block

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
+const gulp = require('gulp'),
+    sass = require('gulp-sass')(require('sass')),
     browserSync = require('browser-sync').create(),
     inky = require('inky'),
     inlineCss = require('gulp-inline-css'),
@@ -7,16 +7,18 @@ var gulp = require('gulp'),
 
 
 //STYLES
-gulp.task('styles', function () {
-  return gulp.src('./scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.reload({stream:true}));
-});
+
+function styles(){
+    return gulp.src('./scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+}
 
 //CONVERTE INKY
-gulp.task('inky', ['styles'], function() {
-  return gulp.src('./templates/**/*.html')
+
+function html(){
+    return gulp.src('./templates/**/*.html')
     .pipe(inlinesource())
     .pipe(inky())
     .pipe(inlineCss({
@@ -24,22 +26,29 @@ gulp.task('inky', ['styles'], function() {
         removeLinkTags: false
     }))
     .pipe(gulp.dest('./dist'));
-});
+}
 
 //BROWSER SYNC
-gulp.task('browser-sync', function () {
+
+function watch() {
     browserSync.init({
         server: {
             baseDir: "./"
         },
         startPath: "/dist"
-    });
-});
-
-//WATCH
-gulp.task('default', ['browser-sync', 'styles', 'inky'], function () {
-    gulp.watch("./scss/*.scss", ['styles']);
-    gulp.watch('./css/*.css', browserSync.reload);
+    })
     gulp.watch('./templates/*.html').on('change', browserSync.reload);
-    gulp.watch(['./scss/*.scss', './templates/*.html'],['inky']);
-});
+    gulp.watch('./templates/*.html', html)
+    gulp.watch("./scss/*.scss", styles);
+    gulp.watch('./css/*.css', browserSync.reload);
+    gulp.watch(['./scss/*.scss', './templates/*.html']);
+}
+
+
+exports.html = html
+exports.styles = styles
+exports.watch = watch
+
+// Таск, который выполняется по команде gulp
+
+exports.default = gulp.series(html, gulp.parallel(styles), watch)
